2e7d2a9cd52cffdb9dd4de70aab42c3f10564d6e
Wanderley Soares Feitosa <wanderleysoares1994@gmail.com>
2025-03-19 18:49:05 +0000

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import Home from './routes/home';
 import { AuthProvider } from './context/useAuth';
 //import PrivateRoute from './components/private_route';
 import Register from './routes/register';
+import Navbar from './components/navbar';
+import Layout from './components/layout';
 
 function App() {
   return (
@@ -19,7 +21,7 @@ function App() {
                         and protecting the Home to not be accessed from Unauthenticated 
                         user. 
               */}
-              <Route path='/' element={<Home />} />
+              <Route path='/' element={<Layout><Home /></Layout>} />
               <Route path='/login' element={<Login />} />
               <Route element={<Register />} path='/register' />
             </Routes>
---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Box, Flex, IconButton, Menu, MenuButton, MenuList, MenuItem, useDisclosure, Heading, Button } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
+
+const Navbar = () => {
+    const { isOpen, onToggle } = useDisclosure();
+    return (
+        <Box bg="blue.500" px={4} position="fixed" top="0" left="0" width="100%" zIndex="1000">
+            <Flex h={16} alignItems="center" justifyContent="space-between">
+                <Heading size="md" color="white">
+                    Auction
+                </Heading>
+                <Flex display={{ base: "none", md: "flex" }} gap={4}>
+                    <Button variant="link" color="white">
+                        Home
+                    </Button>
+                    <Button variant="link" color="white">
+                        About
+                    </Button>
+                    <Button variant="link" color="white">
+                        Contact
+                    </Button>
+                </Flex>
+                <IconButton
+                    size="md"
+                    icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+                    aria-label="Open Menu"
+                    display={{ md: "none" }}
+                    onClick={onToggle}
+                />
+            </Flex>
+            {isOpen && (
+                <Box pb={4} display={{ md: "none" }}>
+                    <Flex direction="column" gap={2}>
+                        <Button variant="link" color="white">
+                            Home
+                        </Button>
+                        <Button variant="link" color="white">
+                            About
+                        </Button>
+                        <Button variant="link" color="white">
+                            Contact
+                        </Button>
+                    </Flex>
+                </Box>
+            )}
+        </Box>
+    );
+};
+
+export default Navbar;
